fa89ba0918ba2b3759e6de5f123c93c9ca2dc923
nasum <tomato.wonder.life@gmail.com>
2022-01-06 22:18:58 +0900

    implement create func for Player struct

--- cmd_game/src/main.rs
+++ cmd_game/src/main.rs
@@ -2,6 +2,13 @@ use std::io;
 
 struct Player {
     name: String,
+    life: i32,
+}
+
+impl Player {
+    fn create(name: String) -> Player {
+        Player { name, life: 100 }
+    }
 }
 
 fn main() {
@@ -14,10 +21,9 @@ fn main() {
         io::stdin()
             .read_line(&mut input)
             .expect("Fail to read line");
-        let player = Player {
-            name: input.trim().to_string(),
-        };
+        let player = Player::create(input.trim().to_string());
 
         println!("Hi, {}!", player.name);
+        println!("Your life is {}.", player.life);
     }
 }
